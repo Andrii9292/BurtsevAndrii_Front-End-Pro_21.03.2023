d4e61c0c1aad5a262b08ddeb29cb39808c88778a
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-03-24 21:59:13 +0900

    wrote functions for all operations

--- js/script.js
+++ js/script.js
@@ -3,24 +3,20 @@ let operand2 = +prompt('Введите второе число:');
 function add(a, b) {
     let result = a + b;
     console.log(`Результат: ${a} + ${b} = ${result}`);
-    alert(`Результат: ${a} + ${b} = ${result}`);
 }
 add(operand1, operand2);
 function subtract(a, b) {
     let result = a - b;
     console.log(`Результат: ${a} - ${b} = ${result}`);
-    alert(`Результат: ${a} - ${b} = ${result}`);
 }
 subtract(operand1, operand2);
 function multiply(a, b) {
     let result = a * b;
     console.log(`Результат: ${a} * ${b} = ${result}`);
-    alert(`Результат: ${a} * ${b} = ${result}`);
 }
 multiply(operand1, operand2);
 function divide(a, b) {
     let result = a / b;
     console.log(`Результат: ${a} / ${b} = ${result}`);
-    alert(`Результат: ${a} / ${b} = ${result}`);
 }
 divide(operand1, operand2);
