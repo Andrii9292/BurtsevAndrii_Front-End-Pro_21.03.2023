a935461a1953267ccc5fe771b254dee3386daa7a
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-04-05 17:04:43 +0900

    test

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ const num1 = +prompt (`введите первое число`);
 const num2 = +prompt (`введите второе число`);
 const action = prompt(`+, -, *, /`, `+,-,*,/`)
 let result;
-    if (`+`)
-        return num1 + num2
-    else if (`-`)
+if (`+`) {
+    if (`-`) {
         return num1 - num2
-    else if (`*`)
+    } if-else (`*`) {
         return num1 * num2
-    else if (`/`)
+    } if-else (`/`) {
         return num1 / num2
+    }
+} else {
+    return num1 + num2
+}
 alert (`${num1} ${action} ${num2} = ${result}`);
