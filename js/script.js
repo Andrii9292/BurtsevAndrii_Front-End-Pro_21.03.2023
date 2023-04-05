b28001ecd96e4cbc3f83aed5760a7a80c5c68dd4
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-04-05 16:45:03 +0900

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+const num1 = +prompt (`введите первое число`);
+const num2 = +prompt (`введите второе число`);
+const action = prompt(`+, -, *, /`, `+,-,*,/`)
+let result;
+function add(result) {
+    if (`+`)
+        return num1 + num2
+    else if (`-`)
+        return num1 - num2
+    else if (`*`)
+        return num1 * num2
+    else if (`/`)
+        return num1 / num2
+
+}
+alert (`${num1} ${action} ${num2} = ${result}`);
